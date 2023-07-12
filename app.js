1fb8d9c8639da2fbe9d240b054f6f7ca0a1195da
erik Griffin <griffin.erikd@gmail.com>
2023-07-12 18:43:54 -0400

    updated exercise

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const searchButton = document.querySelector("SubmitSearch");
+const searchInput = document.querySelector("#searchWord");
+const displayImg = docuemnt.querySelector("#imageContainer > img");
+const mainElement = document.querySelector("main");
+const feedbackP = document.createElement("P");
+mainElement.appendChild(feedbackP);
+
+    searchButton.addEventListener("click" , (event) => {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${myApiKey}&s=${searchInput.value}`)
+    .myApiKey ('JaoVpnTA6VkjcCmBlx3QPGHv9WhPBraK')
+    .then((response) => response.json());
+    })
+    .then((result) =>  {
+        console.log(result);
+        displayImg.src = resulr.data.images.url;
+        searchInput.value = ""
+    })
+    .catch((err) => {
+        console.error(err);
+        feedbackP.textContent = err.mesasge;
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
